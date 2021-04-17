fix padding/off by one in eval harness

mesh_transformer/transformer_shard.py
@@ -107,7 +107,7 @@ class CausalTransformer:
 
             eval_loss_fn = hk.without_apply_rng(hk.transform(eval_loss)).apply
 
-            mask = (jnp.arange(0, len(ctx)) < (len(ctx) - ctx_length - 1)) * -1e10
+            mask = (jnp.arange(0, len(ctx)) < (len(ctx) - ctx_length)) * -1e10
 
             return eval_loss_fn(to_bf16(state["params"]), ctx, tgt, mask)
 

tasks/eval_harness.py
@@ -22,7 +22,7 @@ class EvalHarnessAdaptor(LM):
 
     def request_iter(self, requests):
         for ctx, cont in requests:
-            ctx_tokens = self.tokenizer.encode(ctx)
+            ctx_tokens = self.tokenizer.encode("<|endoftext|>" + ctx)
             cont_tokens = self.tokenizer.encode(cont)
 
             all_tokens = ctx_tokens + cont_tokens
@@ -32,8 +32,8 @@ class EvalHarnessAdaptor(LM):
             pad_amount = self.seq - provided_ctx
 
             yield {
-                "obs": np.pad(all_tokens[:-1], ((pad_amount, 0),)),
-                "target": np.pad(all_tokens[1:], ((pad_amount, 0),)),
+                "obs": np.pad(all_tokens[:-1], ((pad_amount, 0),), constant_values=50256),
+                "target": np.pad(all_tokens[1:], ((pad_amount, 0),), constant_values=50256),
                 "ctx_length": provided_ctx,
                 "eval_mask": np.arange(0, self.seq) > (self.seq - len(cont_tokens) - 1),
             }