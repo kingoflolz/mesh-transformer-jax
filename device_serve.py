68df0ee5994a1dd4be78037bce062acffa22c4c5
Ben Wang <wangben3@gmail.com>
2021-04-24 17:19:36 +1000

    add http serving script for completions

---
+++ device_serve.py
@@ -0,0 +1,166 @@
+import argparse
+import json
+import threading
+import time
+from queue import Queue, Empty
+
+import jax
+import numpy as np
+import optax
+
+from mesh_transformer import util
+from mesh_transformer.checkpoint import read_ckpt
+from mesh_transformer.sampling import nucleaus_sample
+from mesh_transformer.transformer_shard import CausalTransformer
+import transformers
+from smart_open import open
+
+from mesh_transformer.util import clip_by_global_norm
+
+from flask import Flask, request
+app = Flask(__name__)
+
+requests_queue = Queue()
+
+"""
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"context":"eleutherai", "top_p": 0.9, "temp": 0.75}' \
+  http://localhost:5000/complete
+"""
+
+
+@app.route('/complete', methods=['POST'])
+def hello_world():
+    content = request.json
+
+    if requests_queue.qsize() > 100:
+        return {"error": "queue full, try again later"}
+
+    response_queue = Queue()
+
+    requests_queue.put(({
+        "context": content["context"],
+        "top_p": float(content["top_p"]),
+        "temp": float(content["temp"])
+    }, response_queue))
+
+    return {"completion": response_queue.get()}
+
+
+def parse_args():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default=None, help="Config file location")
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    threading.Thread(target=app.run, kwargs={"port": 5000, "host": "0.0.0.0"}).start()
+
+    args = parse_args()
+    params = json.load(open(args.config))
+
+    gradient_accumulation_steps = params.get("gradient_accumulation_steps", 1)
+    per_replica_batch = params["per_replica_batch"]
+    cores_per_replica = params["cores_per_replica"]
+
+    assert cores_per_replica <= 8
+
+    bucket = params["bucket"]
+    model_dir = params["model_dir"]
+    layers = params["layers"]
+    d_model = params["d_model"]
+    n_heads = params["n_heads"]
+    n_vocab = params["n_vocab"]
+    seq = params["seq"]
+    norm = params["norm"]
+
+    params["sampler"] = nucleaus_sample
+    opt = optax.chain(
+        optax.scale(1 / gradient_accumulation_steps),
+        clip_by_global_norm(1),
+        optax.scale_by_adam(),
+        optax.additive_weight_decay(0),
+        optax.scale(-1),
+        optax.scale_by_schedule(util.gpt3_schedule(0, 1, 0, 0))
+    )
+
+    params["optimizer"] = opt
+
+    start = time.time()
+    print(f"jax devices: {jax.device_count()}")
+    print(f"jax runtime initialized in {time.time() - start:.06}s")
+
+    mesh_shape = (jax.device_count() // cores_per_replica, cores_per_replica)
+    devices = np.array(jax.devices()).reshape(mesh_shape)
+
+    with open(f"gs://{bucket}/{model_dir}/meta.json", "r") as f:
+        meta = json.load(f)
+
+    ckpt_step = meta["checkpoints"][-1]
+    print(f"using checkpoint {ckpt_step}")
+
+    total_batch = per_replica_batch * jax.device_count() // cores_per_replica
+    with jax.experimental.maps.mesh(devices, ('dp', 'mp')):
+        network = CausalTransformer(params)
+
+        start = time.time()
+        network.state = read_ckpt(network.state, f"gs://{bucket}/{model_dir}/step_{ckpt_step}/", devices.shape[1])
+        print(f"network loaded in {time.time() - start:.06}s")
+
+        local_shards = max(jax.local_device_count() // mesh_shape[1], 1)
+        del network.state["opt_state"]
+        network.state = network.move_xmap(network.state, np.zeros(local_shards))
+
+        tokenizer = transformers.GPT2TokenizerFast.from_pretrained('gpt2')
+
+        while True:
+            all_ctx = []
+            all_top_p = []
+            all_temp = []
+            all_q = []
+            while len(all_ctx) < total_batch:
+                try:
+                    o, q = requests_queue.get(block=False)
+                    all_ctx.append(o["context"])
+                    all_top_p.append(o["top_p"])
+                    all_temp.append(o["temp"])
+                    all_q.append(q)
+                except Empty:
+                    if len(all_ctx):
+                        break
+                    else:
+                        time.sleep(0.01)
+
+            start = time.time()
+            while len(all_ctx) < total_batch:
+                all_ctx.append("whatever")
+                all_top_p.append(1)
+                all_temp.append(1)
+
+            all_tokenized = []
+            all_length = []
+            for ctx in all_ctx:
+                tokens = tokenizer.encode(ctx)
+                provided_ctx = len(tokens)
+                pad_amount = seq - provided_ctx
+
+                padded_tokens = np.pad(tokens, ((pad_amount, 0),)).astype(np.uint32)[-seq:]
+                all_tokenized.append(padded_tokens)
+                all_length.append(len(tokens))
+
+            output = network.generate(np.array(all_tokenized),
+                                      np.array(all_length),
+                                      512,
+                                      {
+                                          "top_p": np.array(all_top_p),
+                                          "temp": np.array(all_temp)
+                                      })
+
+            for o, q in zip(output[1][0][:, :, 0], all_q):
+                q.put(tokenizer.decode(o))
+
+            print(f"completion done in {time.time() - start:06}s")
