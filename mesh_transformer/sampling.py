56626ff9176f18c13ef61b7f10a6335a1408525f
AeroScripts <9335832+AeroScripts@users.noreply.github.com>
2021-06-11 00:58:57 -0400

    Implement top_k sampling (#13)

--- mesh_transformer/sampling.py
+++ mesh_transformer/sampling.py
@@ -7,10 +7,23 @@ def softmax_sample(key, logits, _, temp=1):
     return jax.random.categorical(key, logits/temp, -1).astype(jnp.uint32), None
 
 
-def nucleaus_filter(logits, top_p=0.9):
+def nucleaus_filter(logits, top_p=0.9, top_k=None):
     sorted_logits = jnp.sort(logits)[:, ::-1] # sort descending
     sorted_indices = jnp.argsort(logits)[:, ::-1]
     cumulative_probs = jnp.cumsum(jax.nn.softmax(sorted_logits), axis=-1)
+
+    if top_k is not None:
+        # Keep only top_k tokens
+        indices_range = jnp.arange(len(sorted_indices[0]))
+        indices_range = jnp.stack([indices_range] * len(sorted_indices), axis=0)
+
+        sorted_indices_to_remove = jnp.where(indices_range > top_k, sorted_indices, 0)
+
+        _, indices_to_remove = jax.lax.sort_key_val(sorted_indices, sorted_indices_to_remove)
+
+        logit_mask = 1e10 * indices_to_remove
+
+        logits -= logit_mask
 
     # Remove tokens with cumulative probability above a threshold
     sorted_indices_to_remove = cumulative_probs > top_p
@@ -25,8 +38,8 @@ def nucleaus_filter(logits, top_p=0.9):
     return logits
 
 
-def nucleaus_sample(key, logits, _, top_p=0.9, temp=1):
-    logits = nucleaus_filter(logits, top_p)
+def nucleaus_sample(key, logits, _, top_p=0.9, temp=1, top_k=None):
+    logits = nucleaus_filter(logits, top_p, top_k=top_k)
 
     return softmax_sample(key, logits, None, temp=temp)
 
