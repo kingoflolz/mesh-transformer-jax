03d75cbefda71d5c52469ec96db3d4b5a7af5ecf
yousefkami <49797583+yousefkami@users.noreply.github.com>
2021-07-05 18:41:46 -0400

    Commit Exodia
     this is Exodia

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='mesh_transformer',
-    version='0.0.0',
-    packages=find_packages(include=['mesh_transformer', 'mesh_transformer.*'])
-)
