48f00e6a8bb21aaa830876730cfb62dd379679f8
Ben Wang <wangben3@gmail.com>
2021-07-25 14:44:45 -0700

    add hack to prevent cloud tpu python version upgrades

--- scripts/init_ray_v2.sh
+++ scripts/init_ray_v2.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# this locks the python executable down to hopefully stop if from being fiddled with...
+screen -d -m python -c 'import time; time.sleep(999999999)'
+
 # initializes jax and installs ray on cloud TPUs
-pip install "jax[tpu]>=0.2.18" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+sudo pip install "jax[tpu]>=0.2.18" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 sudo pip install --upgrade ray[default]==1.4.1 fabric dataclasses optax git+https://github.com/deepmind/dm-haiku tqdm cloudpickle smart_open[gcs] einops func_timeout
